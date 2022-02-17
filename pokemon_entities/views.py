a1a257c80c6d26517f5e7f872a8b2a8d3f05fcae
b10t <b10t.rus@gmail.com>
2022-02-17 09:08:21 +0800

    Show title pokemon on japan and english language.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -76,6 +76,8 @@ def show_pokemon(request, pokemon_id):
         'pokemon_id': pokemon.id,
         'img_url': get_image_url(request, pokemon.image),
         'title_ru': pokemon.title,
+        'title_en': pokemon.title_en,
+        'title_jp': pokemon.title_jp,
         'description': pokemon.description
     }
 
